784903248312f5a715575b9ee5086105a9c14678
Kinolog76 <vladpasha12@gmail.com>
2025-03-25 13:27:49 +0200

    .

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -1,52 +1,55 @@
-const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const TARGET_TABLE = process.env.target_table;
 
 exports.handler = async (event) => {
-    console.log("Received event:", JSON.stringify(event, null, 2));
+  console.log("Received event:", JSON.stringify(event, null, 2));
 
-    const auditPromises = event.Records.map(async (record) => {
-        const currentTime = new Date().toISOString();
+  const auditPromises = event.Records.map(async (record) => {
+    const currentTime = new Date().toISOString();
 
-        const { eventName, dynamodb } = record;
-        const keys = dynamodb.Keys.key.S;
+    const { eventName, dynamodb } = record;
+    const keys = dynamodb.Keys.key.S;
 
-        let auditItem = {
-            id: uuidv4(),
-            itemKey: keys,
-            modificationTime: currentTime
-        };
+    let auditItem = {
+      id: uuidv4(),
+      itemKey: keys,
+      modificationTime: currentTime,
+    };
 
-        if (eventName === "INSERT") {
-            auditItem.newValue = dynamodb.NewImage;
-        } else if (eventName === "MODIFY") {
-            auditItem.oldValue = dynamodb.OldImage.value.N;
-            auditItem.newValue = dynamodb.NewImage.value.N;
-        } else {
-            console.log(`Skipping unsupported event type: ${eventName}`);
-            return;
-        }
-
-        const params = {
-            TableName: TARGET_TABLE,
-            Item: auditItem
-        };
-
-        console.log("Saving audit record:", JSON.stringify(params, null, 2));
-        return dynamoDB.put(params).promise();
-    });
-
-    try {
-        await Promise.all(auditPromises);
-        console.log("Successfully processed all records.");
-    } catch (error) {
-        console.error("Error saving audit records:", error);
+    if (eventName === "INSERT") {
+      auditItem.newValue = {
+        key: dynamodb.NewImage.key.S,
+        value: Number(dynamodb.NewImage.value.N),
+      };
+    } else if (eventName === "MODIFY") {
+      auditItem.oldValue = Number(dynamodb.OldImage.value.N);
+      auditItem.newValue = Number(dynamodb.NewImage.value.N);
+    } else {
+      console.log(`Skipping unsupported event type: ${eventName}`);
+      return;
     }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Processed successfully." })
+    const params = {
+      TableName: TARGET_TABLE,
+      Item: auditItem,
     };
+
+    console.log("Saving audit record:", JSON.stringify(params, null, 2));
+    return dynamoDB.put(params).promise();
+  });
+
+  try {
+    await Promise.all(auditPromises);
+    console.log("Successfully processed all records.");
+  } catch (error) {
+    console.error("Error saving audit records:", error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Processed successfully." }),
+  };
 };
